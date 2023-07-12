63a5e701efd9cb722680179bf63767e6d7efd2e9
Oskar <obiwejnis@airnauts.com>
2023-07-12 11:14:15 +0200

    Implement loadView(), create enum Constants

--- WeatherApp/WelcomeView.swift
+++ WeatherApp/WelcomeView.swift
@@ -23,10 +23,20 @@ class WelcomeView: UIView {
         static let proceedButtonName = "Proceed"
     }
     
+    private enum Constants {
+        static let iconImageViewCornerRadius = 40
+        static let proceedButtonCornerRadius = 20
+        static let proceedButtonToTitleLabelDistance = 300
+        static let proceedButtonWidth = 150
+        static let proceedButtonHeight = 50
+        static let titleLabelYOffset = -50
+        static let iconImageViewToTitleLabelDistance = -1
+        static let iconImageSize = 300
+    }
     private let iconImageView: UIImageView = {
         let iconImageView = UIImageView(image: UIImage(systemName: Strings.systemIconName) )
         iconImageView.translatesAutoresizingMaskIntoConstraints = false
-        iconImageView.layer.cornerRadius = 40
+        iconImageView.layer.cornerRadius = CGFloat(Constants.iconImageViewCornerRadius)
         iconImageView.tintColor = .systemCyan
         iconImageView.backgroundColor = .systemGray3
         return iconImageView
@@ -46,7 +56,7 @@ class WelcomeView: UIView {
     private let proceedButton: UIButton = {
         let proceedButton = UIButton(type: .system)
         proceedButton.backgroundColor = .systemGray2
-        proceedButton.layer.cornerRadius = 20
+        proceedButton.layer.cornerRadius = CGFloat(Constants.proceedButtonCornerRadius)
         proceedButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title3)
         proceedButton.setTitle(Strings.proceedButtonName, for: .normal)
         proceedButton.translatesAutoresizingMaskIntoConstraints = false
@@ -70,18 +80,17 @@ class WelcomeView: UIView {
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             proceedButton.centerXAnchor.constraint(equalTo: self.layoutMarginsGuide.centerXAnchor),
-            proceedButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor, constant: 300),
-            proceedButton.widthAnchor.constraint(equalToConstant: 150),
-            proceedButton.heightAnchor.constraint(equalToConstant: 50),
+            proceedButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor, constant: CGFloat(Constants.proceedButtonToTitleLabelDistance)),
+            proceedButton.widthAnchor.constraint(equalToConstant: CGFloat(Constants.proceedButtonWidth)),
+            proceedButton.heightAnchor.constraint(equalToConstant: CGFloat(Constants.proceedButtonHeight)),
             
             titleLabel.centerXAnchor.constraint(equalTo: self.layoutMarginsGuide.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: self.layoutMarginsGuide.centerYAnchor, constant: -50),
+            titleLabel.centerYAnchor.constraint(equalTo: self.layoutMarginsGuide.centerYAnchor, constant: CGFloat(Constants.titleLabelYOffset)),
             
             iconImageView.centerXAnchor.constraint(equalTo: self.layoutMarginsGuide.centerXAnchor),
-            iconImageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -10),
-            iconImageView.widthAnchor.constraint(equalToConstant: 300),
-            iconImageView.heightAnchor.constraint(equalToConstant: 300),
+            iconImageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: CGFloat(Constants.iconImageViewToTitleLabelDistance)),
+            iconImageView.widthAnchor.constraint(equalToConstant: CGFloat(Constants.iconImageSize)),
+            iconImageView.heightAnchor.constraint(equalToConstant: CGFloat(Constants.iconImageSize)),
         ])
     }
-    
 }
--- WeatherApp/WelcomeViewController.swift
+++ WeatherApp/WelcomeViewController.swift
@@ -2,13 +2,14 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    var welcomeView = WelcomeView()
     
+    override func loadView() {
+        super.loadView()
+        view = welcomeView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view = WelcomeView()
     }
-
-    
 }
