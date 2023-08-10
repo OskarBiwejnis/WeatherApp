be61fa6dcaf18430da59e123e7bff8812fc1baef
Oskar <obiwejnis@airnauts.com>
2023-08-10 11:46:21 +0200

    Add tests for forecast module, renamed tests in search module

--- WeatherAppTests/SearchViewModelTests.swift
+++ WeatherAppTests/SearchViewModelTests.swift
@@ -62,61 +62,62 @@ final class SearchViewModelTests: XCTestCase {
 
 
 
-    func testShouldShowErrorInvalidUrlWhenSearchTextDidChangeWithIncorrectText() throws {
+    func testShouldShowErrorWhenSearchTextDidChangeWithIncorrectText() throws {
         XCTAssertFalse(mockSearchViewModelDelegate.didCallShowError)
         let expectation = self.expectation(description: "Show Error")
         mockSearchViewModelDelegate.expectationShowError = expectation
 
-        searchViewModel.searchTextDidChange("6^& ćż [[")
+        searchViewModel.searchTextDidChange("ThisStringThrowsError")
 
         waitForExpectations(timeout: 5)
         XCTAssertTrue(mockSearchViewModelDelegate.didCallShowError)
+        XCTAssert(mockSearchViewModelDelegate.error is MockError)
+    }
+
+    class MockSearchViewModelDelegate: SearchViewModelDelegate {
+
+        var didCallOpenCityForecast = false
+        var didCallReloadTable = false
+        var didCallShowError = false
+        var city: City?
+        var expectationReloadTable: XCTestExpectation?
+        var expectationShowError: XCTestExpectation?
+        var error: Error?
+
+        func openCityForecast(city: City) {
+            didCallOpenCityForecast = true
+            self.city = city
+        }
+
+        func reloadTable() {
+            didCallReloadTable = true
+            expectationReloadTable?.fulfill()
+        }
+
+        func showError(_ error: Error) {
+            didCallShowError = true
+            expectationShowError?.fulfill()
+            self.error = error
+        }
+
+    }
+
+    class MockNetworkingService: NetworkingServiceType {
+
+        func fetchCities(_ searchText: String) async throws -> [City] {
+            if searchText == "ThisStringThrowsError" { throw MockError() }
+            return []
+        }
+
+        func fetchThreeHourForecast(city: WeatherApp.City) async throws -> [ThreeHourForecast] {
+            return []
+        }
+
+    }
+
+    class MockError: Error {
+
     }
 
 }
 
-class MockSearchViewModelDelegate: SearchViewModelDelegate {
-
-    var didCallOpenCityForecast = false
-    var didCallReloadTable = false
-    var didCallShowError = false
-    var city: City?
-    var expectationReloadTable: XCTestExpectation?
-    var expectationShowError: XCTestExpectation?
-    var error: Error?
-
-    func openCityForecast(city: City) {
-        didCallOpenCityForecast = true
-        self.city = city
-    }
-
-    func reloadTable() {
-        didCallReloadTable = true
-        expectationReloadTable?.fulfill()
-    }
-
-    func showError(_ error: Error) {
-        didCallShowError = true
-        expectationShowError?.fulfill()
-        self.error = error
-    }
-
-}
-
-class MockNetworkingService: NetworkingServiceType {
-
-    func fetchCities(_ searchText: String) async throws -> [City] {
-        guard let url = URL(string: "www.google.com/" + searchText) else { throw MockError() }
-        return []
-    }
-
-    func fetchThreeHourForecast(city: WeatherApp.City) async throws -> [ThreeHourForecast] {
-        return []
-    }
-
-
-}
-
-class MockError: Error {
-
-}
