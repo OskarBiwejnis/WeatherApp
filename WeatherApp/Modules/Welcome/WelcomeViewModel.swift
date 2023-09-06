a23038279127f7d2341c7fb7db15f55e3e135762
Oskar <obiwejnis@airnauts.com>
2023-09-06 17:28:16 +0200

    Minor change

--- WeatherApp/Modules/Welcome/WelcomeViewModel.swift
+++ WeatherApp/Modules/Welcome/WelcomeViewModel.swift
@@ -35,8 +35,6 @@ class WelcomeViewModel: WelcomeViewModelContract {
         .compactMap { [weak self] event in
             if case .viewWillAppear = event {
                 let recentCities = self?.storageService.getRecentCities() ?? []
-                print("Recent", recentCities)
-                print("Ze storaga bezposr", self?.storageService.getRecentCities())
                 self?.recentCities = recentCities
                 return recentCities
             } else { return nil }
